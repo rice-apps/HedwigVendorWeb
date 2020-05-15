b8dfa1e5da88275026d9b31bd6428a674a422dd5
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-05-14 20:41:44 -0400

    Added Apollo Client v3

--- src/index.js
+++ src/index.js
@@ -5,4 +5,19 @@ import {render} from 'react-dom'
 
 import App from './App'
 
-render(<App/>, document.querySelector('#app'))
+// Apollo Client Setup
+import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+  })
+});
+
+render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.querySelector("#app")
+)
